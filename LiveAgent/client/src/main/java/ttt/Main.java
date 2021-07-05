c83a0ea959469b02cbd347a6927f440fc7778396
Vasyl Tsalko <vtsalko@qualityunit.com>
2021-07-05 12:07:02 +0300

    master

--- LiveAgent/client/src/main/java/ttt/Main.java
+++ LiveAgent/client/src/main/java/ttt/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String args[]) {
 
         for (int i = 5; i >= 0; i++)
-            System.out.println("Hello: " + i);
+            System.out.println("Master: " + i);
 
         System.out.println("Fuck the rest");
     }
